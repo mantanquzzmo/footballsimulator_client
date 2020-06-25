e36b59f20a70c8d123eacba7eaf16d5bf98391a2
mantanquzzmo <quzzmo@hotmail.com>
2020-06-24 17:41:13 -0700

    first request calls created

---
+++ src/modules/backend_calls.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const createTeam = async (name, primaryColor, secondaryColor) => {
+  try {
+    const response = await axios({
+      method: "POST",
+      url: "api/teams",
+      params: {
+        name: name,
+        primary_color: primaryColor,
+        secondary_color: secondaryColor
+      }
+    })
+    return response
+  } catch (error) {
+    return error
+    }
+  }
+
+
+
+export { createTeam }
