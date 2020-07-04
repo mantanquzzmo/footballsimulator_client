55de305a91674fa875d682d44bf1e64471853f8a
mantanquzzmo <quzzmo@hotmail.com>
2020-07-04 09:05:45 -0700

    adds edit button after is created

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { requestTeam } from "../modules/backend_calls.jsx";
 import { drawShirt } from "../helpers/drawShirt";
 
 const Teams = (props) => {
+  const [visibility, setVisibility] = useState("hidden");
   const createTeam = async (e) => {
     e.preventDefault();
     const teamName = e.target.teamName.value;
@@ -17,6 +18,26 @@ const Teams = (props) => {
       props.createdTeamInfo(team.data[0]);
       props.createdPlayersInfo(team.data[1]);
       props.createTeamProgression(1);
+      drawShirt(primaryColor, secondaryColor);
+    }
+  };
+
+  const updateTeam = async (e) => {
+    e.preventDefault();
+    const teamName = e.target.teamName2.value;
+    const primaryColor = e.target.primaryColor2.value;
+    const secondaryColor = e.target.secondaryColor2.value;
+
+    const updatedTeam = await requestTeam(
+      teamName,
+      primaryColor,
+      secondaryColor
+    );
+    if (updatedTeam.error) {
+      props.changeMessage(updatedTeam.error);
+    } else {
+      debugger;
+      props.createdTeamInfo(updatedTeam.data[0]);
       drawShirt(primaryColor, secondaryColor);
     }
   };
@@ -45,6 +66,25 @@ const Teams = (props) => {
           Secondary Color:
           {props.secondaryColor}
           <canvas id="teamColors"></canvas>
+          <button
+            onClick={() => {
+              setVisibility("visible");
+            }}
+          >
+            Edit team colors
+          </button>
+
+          <div style={{ visibility: visibility }}>
+          <form onSubmit={(e) => updateTeam(e)}>
+            TeamName:
+            <input name="teamName2" id="teamName2"></input>
+            Primary Color:
+            <input name="primaryColor2" id="primaryColor2"></input>
+            Secondary Color:
+            <input name="secondaryColor2" id="secondaryColor2"></input>
+            <button id="teamSubmit">submit your team</button>
+          </form>
+        </div>
         </div>
       )}
     </div>
